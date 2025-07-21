a4e21cc1b4dc6657a32960bcb54e6936b35bbbd0
Dell Inspiron 15 <Dell Inspiron 15@DESKTOP-NQ3CE08>
2025-07-21 10:19:32 -0700

    addition

---
+++ C2TC_CoreJavaProgram/src/Day1/Basics/SampleDemo.java
@@ -0,0 +1,9 @@
+package Day1.Basics;
+
+public class SampleDemo {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Welcome to JES....");
+
+	}
+}
