c99f80c7406717ebe90a686eb74009a81b74cb2d
Dell Inspiron 15 <Dell Inspiron 15@DESKTOP-NQ3CE08>
2025-07-21 20:58:58 -0700

    day 2 conditional and looping statement program

--- C2TC_CoreJavaProgram/src/Day1/Basics/SampleDemo.java
+++
@@ -1,9 +0,0 @@
-package Day1.Basics;
-
-public class SampleDemo {
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.print("Welcome to JES....");
-
-	}
-}
