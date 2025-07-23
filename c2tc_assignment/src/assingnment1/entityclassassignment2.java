68f4b1f32d0c4d477171690ef2ed84bb0bcfd538
Dell Inspiron 15 <Dell Inspiron 15@DESKTOP-NQ3CE08>
2025-07-22 22:34:41 -0700

    program

---
+++ c2tc_assignment/src/assingnment1/entityclassassignment2.java
@@ -0,0 +1,16 @@
+package assingnment1;
+
+public class entityclassassignment2 {
+	  public static void main(String[] args) {
+	        Student s1 = new Student("Gokul", 20, "IT");
+	        Student s2 = new Student("Anu", 21, "CSE");
+	        Student s3 = new Student("Raj", 22, "ECE");
+
+	        System.out.println("Student Details:");
+	        s1.display();
+	        s2.display();
+	        s3.display();
+	    }
+
+
+}
